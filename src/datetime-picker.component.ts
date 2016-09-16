3e0d36649e7af863207b6d39811c5eca09b1a83d
Martin Lonský <martin.lonsky@appin.cz>
2016-09-16 15:11:32 +0200

    keep formatting

--- src/datetime-picker.component.ts
+++ src/datetime-picker.component.ts
@@ -6,8 +6,8 @@ import {
   EventEmitter,
   ViewChild,
   AfterViewInit
-} from "@angular/core";
-import {DateTime} from "./datetime";
+} from '@angular/core';
+import {DateTime} from './datetime';
 
 //@TODO
 // . display currently selected day
@@ -16,9 +16,9 @@ import {DateTime} from "./datetime";
  * show a selected date in monthly calendar
  */
 @Component({
-  providers: [DateTime],
-  selector: 'datetime-picker',
-  template: `
+  providers    : [DateTime],
+  selector     : 'datetime-picker',
+  template     : `
 <div class="datetime-picker" tabindex="0">
 
   <!-- Month - Year  -->
@@ -95,7 +95,7 @@ import {DateTime} from "./datetime";
 <!--Date: {{selectedDate}}<br/>-->
 <!--Hour: {{hour}} Minute: {{minute}}<br/>-->
   `,
-  styles: [
+  styles       : [
     `
  @keyframes slideDown {
   0% {
@@ -267,9 +267,9 @@ export class DateTimePickerComponent implements AfterViewInit {
 
   public initDateTime (date:Date) {
     this.selectedDate = date;
-    this.hour = this.selectedDate.getHours();
-    this.minute = this.selectedDate.getMinutes();
-    this.monthData = this.dateTime.getMonthData(this.year, this.month);
+    this.hour         = this.selectedDate.getHours();
+    this.minute       = this.selectedDate.getMinutes();
+    this.monthData    = this.dateTime.getMonthData(this.year, this.month);
   }
 
   public toDate (year:number, month:number, day:number):Date {
@@ -290,8 +290,8 @@ export class DateTimePickerComponent implements AfterViewInit {
     }
     this.changes.emit({
       selectedDate: this.selectedDate,
-      hour: this.hour,
-      minute: this.minute
+      hour        : this.hour,
+      minute      : this.minute
     });
     this.closing.emit(true);
   };
