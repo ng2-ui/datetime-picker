d0792f6e0fbeb61bfc91f2076b3f2b871505f82f
Allen Kim <allenhwkim@gmail.com>
2016-09-17 17:30:57 -0400

    0.8.3

--- dist/datetime-picker.component.js
+++ dist/datetime-picker.component.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
-var datetime_1 = require("./datetime");
+var core_1 = require('@angular/core');
+var datetime_1 = require('./datetime');
 //@TODO
 // . display currently selected day
 /**
