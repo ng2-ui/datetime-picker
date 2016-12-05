4b778cb669d4aadb013374e14b8e0ad381cd0735
Allen Kim <allenhwkim@gmail.com>
2016-12-05 11:45:08 -0500

    0.11.0

--- dist/datetime-picker.component.js
+++ dist/datetime-picker.component.js
@@ -19,19 +19,12 @@ var DateTimePickerComponent = (function () {
         }
     }
     DateTimePickerComponent.prototype.ngAfterViewInit = function () {
+        var stopPropagation = function (e) { return e.stopPropagation(); };
         if (!this.dateOnly) {
-            this._hours.nativeElement.addEventListener('keyup', function (e) {
-                e.stopPropagation();
-            });
-            this._hours.nativeElement.addEventListener('mousedown', function (e) {
-                e.stopPropagation();
-            });
-            this._minutes.nativeElement.addEventListener('keyup', function (e) {
-                e.stopPropagation();
-            });
-            this._minutes.nativeElement.addEventListener('mousedown', function (e) {
-                e.stopPropagation();
-            });
+            this._hours.nativeElement.addEventListener('keyup', stopPropagation);
+            this._hours.nativeElement.addEventListener('mousedown', stopPropagation);
+            this._minutes.nativeElement.addEventListener('keyup', stopPropagation);
+            this._minutes.nativeElement.addEventListener('mousedown', stopPropagation);
         }
     };
     Object.defineProperty(DateTimePickerComponent.prototype, "year", {
@@ -72,14 +65,13 @@ var DateTimePickerComponent = (function () {
         configurable: true
     });
     DateTimePickerComponent.prototype.initDateTime = function (date) {
-        console.log('this.defaultValue', this.defaultValue);
         this.selectedDate = date || this.defaultValue || new Date();
         this.hour = this.selectedDate.getHours();
         this.minute = this.selectedDate.getMinutes();
         this.monthData = this.dateTime.getMonthData(this.year, this.month);
     };
-    DateTimePickerComponent.prototype.toDate = function (year, month, day) {
-        return new Date(year, month, day);
+    DateTimePickerComponent.prototype.toDate = function (day, month) {
+        return new Date(this.monthData.year, month || this.monthData.month, day);
     };
     DateTimePickerComponent.prototype.toDateOnly = function (date) {
         return new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0);
@@ -88,9 +80,10 @@ var DateTimePickerComponent = (function () {
      * set the selected date and close it when closeOnSelect is true
      * @param date {Date}
      */
-    DateTimePickerComponent.prototype.selectDate = function (dayNum) {
-        if (dayNum) {
-            this.selectedDate = new Date(this.monthData.year, this.monthData.month, dayNum);
+    DateTimePickerComponent.prototype.selectDate = function (date) {
+        this.selectedDate = date || this.selectedDate;
+        if (this.isDateDisabled(this.selectedDate)) {
+            return false;
         }
         this.selectedDate.setHours(parseInt('' + this.hour || '0', 10));
         this.selectedDate.setMinutes(parseInt('' + this.minute || '0', 10));
@@ -104,11 +97,28 @@ var DateTimePickerComponent = (function () {
     DateTimePickerComponent.prototype.updateMonthData = function (num) {
         this.monthData = this.dateTime.getMonthData(this.monthData.year, this.monthData.month + num);
     };
+    DateTimePickerComponent.prototype.isDateDisabled = function (date) {
+        var dateInTime = date.getTime();
+        var minDateInTime = this.minDate.getTime();
+        var maxDateInTime = this.maxDate.getTime();
+        this.disabledDatesInTime =
+            this.disabledDatesInTime || (this.disabledDates || []).map(function (d) { return d.getTime(); });
+        if (this.minDate && (dateInTime < minDateInTime)) {
+            return true;
+        }
+        else if (this.maxDate && (dateInTime > maxDateInTime)) {
+            return true;
+        }
+        else if (this.disabledDatesInTime.indexOf(dateInTime) >= 0) {
+            return true;
+        }
+        return false;
+    };
     DateTimePickerComponent.decorators = [
         { type: core_1.Component, args: [{
                     providers: [datetime_1.DateTime],
                     selector: 'datetime-picker',
-                    template: "\n<div class=\"datetime-picker\" tabindex=\"0\">\n\n  <!-- Month - Year  -->\n  <div class=\"month\" *ngIf=\"!timeOnly\">\n    <b class=\"prev_next prev\" (click)=\"updateMonthData(-12)\">&laquo;</b>\n    <b class=\"prev_next prev\" (click)=\"updateMonthData(-1)\">&lsaquo;</b>\n     <span title=\"{{dateTime.months[monthData.month]?.fullName}}\">\n           {{dateTime.months[monthData.month]?.shortName}}\n     </span>\n    {{monthData.year}}\n    <b class=\"prev_next next\" (click)=\"updateMonthData(+12)\">&raquo;</b>\n    <b class=\"prev_next next\" (click)=\"updateMonthData(+1)\">&rsaquo;</b>\n  </div>\n\n  <!-- Date -->\n  <div class=\"days\" *ngIf=\"!timeOnly\">\n\n    <!-- Su Mo Tu We Th Fr Sa -->\n    <div class=\"day-of-week\"\n         *ngFor=\"let dayOfWeek of dateTime.localizedDaysOfWeek\"\n         [ngClass]=\"{weekend: dayOfWeek.weekend}\"\n         title=\"{{dayOfWeek.fullName}}\">\n      {{dayOfWeek.shortName}}\n    </div>\n\n    <!-- Fill up blank days for this month -->\n    <div *ngIf=\"monthData.leadingDays.length < 7\">\n      <div class=\"day\" *ngFor=\"let dayNum of monthData.leadingDays\"\n           [ngClass]=\"{weekend: [0,6].indexOf(toDate(monthData.year, monthData.month-1, dayNum).getDay()) !== -1}\">\n        {{dayNum}}\n      </div>\n    </div>\n\n    <div class=\"day selectable\"\n         *ngFor=\"let dayNum of monthData.days\"\n         (click)=\"selectDate(dayNum)\"\n         title=\"{{monthData.year}}-{{monthData.month+1}}-{{dayNum}}\"\n         [ngClass]=\"{\n           selected:\n             toDate(monthData.year, monthData.month, dayNum).getTime() === toDateOnly(selectedDate).getTime(),\n           today:\n             toDate(monthData.year, monthData.month, dayNum).getTime() === today.getTime(),\n           weekend:\n             [0,6].indexOf(toDate(monthData.year, monthData.month, dayNum).getDay()) !== -1\n         }\">\n      {{dayNum}}\n    </div>\n\n    <!-- Fill up blank days for this month -->\n    <div *ngIf=\"monthData.trailingDays.length < 7\">\n      <div class=\"day\"\n           *ngFor=\"let dayNum of monthData.trailingDays\"\n           [ngClass]=\"{weekend: [0,6].indexOf(toDate(monthData.year, monthData.month+1, dayNum).getDay()) !== -1}\">\n        {{dayNum}}\n      </div>\n    </div>\n  </div>\n\n  <!-- Time -->\n  <div class=\"days\" id=\"time\" *ngIf=\"!dateOnly\">\n    <label class=\"timeLabel\">Time:</label>\n    <span class=\"timeValue\">\n      {{(\"0\"+hour).slice(-2)}} : {{(\"0\"+minute).slice(-2)}}\n    </span><br/>\n    <label class=\"hourLabel\">Hour:</label>\n    <input #hours class=\"hourInput\"\n           (change)=\"selectDate()\"\n           type=\"range\" min=\"0\" max=\"23\" [(ngModel)]=\"hour\" />\n    <label class=\"minutesLabel\">Min:</label>\n    <input #minutes class=\"minutesInput\"\n           step=\"{{minuteStep}}\"\n           (change)=\"selectDate()\"\n           type=\"range\" min=\"0\" max=\"59\" range=\"10\" [(ngModel)]=\"minute\"/>\n  </div>\n</div>\n\n<!--<hr/>-->\n<!--Date: {{selectedDate}}<br/>-->\n<!--Hour: {{hour}} Minute: {{minute}}<br/>-->\n  ",
+                    template: "\n<div class=\"datetime-picker\" tabindex=\"0\">\n\n  <!-- Month - Year  -->\n  <div class=\"month\" *ngIf=\"!timeOnly\">\n    <b class=\"prev_next prev\" (click)=\"updateMonthData(-12)\">&laquo;</b>\n    <b class=\"prev_next prev\" (click)=\"updateMonthData(-1)\">&lsaquo;</b>\n     <span title=\"{{dateTime.months[monthData.month]?.fullName}}\">\n           {{dateTime.months[monthData.month]?.shortName}}\n     </span>\n    {{monthData.year}}\n    <b class=\"prev_next next\" (click)=\"updateMonthData(+12)\">&raquo;</b>\n    <b class=\"prev_next next\" (click)=\"updateMonthData(+1)\">&rsaquo;</b>\n  </div>\n\n  <!-- Date -->\n  <div class=\"days\" *ngIf=\"!timeOnly\">\n\n    <!-- Su Mo Tu We Th Fr Sa -->\n    <div class=\"day-of-week\"\n         *ngFor=\"let dayOfWeek of dateTime.localizedDaysOfWeek\"\n         [ngClass]=\"{weekend: dayOfWeek.weekend}\"\n         title=\"{{dayOfWeek.fullName}}\">\n      {{dayOfWeek.shortName}}\n    </div>\n\n    <!-- Fill up blank days for this month -->\n    <div *ngIf=\"monthData.leadingDays.length < 7\">\n      <div class=\"day\" *ngFor=\"let dayNum of monthData.leadingDays\"\n           [ngClass]=\"{weekend: [0,6].indexOf(toDate(dayNum, monthData.month-1).getDay()) !== -1}\">\n        {{dayNum}}\n      </div>\n    </div>\n\n    <div class=\"day\"\n         *ngFor=\"let dayNum of monthData.days\"\n         (click)=\"selectDate(toDate(dayNum))\"\n         title=\"{{monthData.year}}-{{monthData.month+1}}-{{dayNum}}\"\n         [ngClass]=\"{\n           selectable: !isDateDisabled(toDate(dayNum)),\n           selected:\n             toDate(dayNum).getTime() === toDateOnly(selectedDate).getTime(),\n           today:\n             toDate(dayNum).getTime() === today.getTime(),\n           weekend:\n             [0,6].indexOf(toDate(dayNum).getDay()) !== -1\n         }\">\n      {{dayNum}}\n    </div>\n\n    <!-- Fill up blank days for this month -->\n    <div *ngIf=\"monthData.trailingDays.length < 7\">\n      <div class=\"day\"\n           *ngFor=\"let dayNum of monthData.trailingDays\"\n           [ngClass]=\"{weekend: [0,6].indexOf(toDate(dayNum, monthData.month+1).getDay()) !== -1}\">\n        {{dayNum}}\n      </div>\n    </div>\n  </div>\n\n  <!-- Time -->\n  <div class=\"days\" id=\"time\" *ngIf=\"!dateOnly\">\n    <label class=\"timeLabel\">Time:</label>\n    <span class=\"timeValue\">\n      {{(\"0\"+hour).slice(-2)}} : {{(\"0\"+minute).slice(-2)}}\n    </span><br/>\n    <label class=\"hourLabel\">Hour:</label>\n    <input #hours class=\"hourInput\"\n           (change)=\"selectDate()\"\n           type=\"range\"\n           min=\"{{minHour || 0}}\"\n           max=\"{{maxHour || 23}}\"\n           [(ngModel)]=\"hour\" />\n    <label class=\"minutesLabel\">Min:</label>\n    <input #minutes class=\"minutesInput\"\n           step=\"{{minuteStep}}\"\n           (change)=\"selectDate()\"\n           type=\"range\" min=\"0\" max=\"59\" range=\"10\" [(ngModel)]=\"minute\"/>\n  </div>\n</div>\n  ",
                     styles: [
                         "\n @keyframes slideDown {\n  0% {\n    transform:  translateY(-10px);\n  }\n  100% {\n    transform: translateY(0px);\n  }\n}\n\n.ng2-datetime-picker {\n    position: relative;\n}\n\n.datetime-picker {\n    color: #333;\n    outline-width: 0;\n    font: normal 14px sans-serif;\n    border: 1px solid #ddd;\n    display: inline-block;\n    background: #fff;\n    animation: slideDown 0.1s ease-in-out;\n    animation-fill-mode: both;\n}\n.datetime-picker > .month {\n    text-align: center;\n    line-height: 22px;\n    padding: 10px;\n    background: #fcfcfc;\n    text-transform: uppercase;\n    font-weight: bold;\n    border-bottom: 1px solid #ddd;\n    position: relative;\n}\n.datetime-picker > .month > .prev_next {\n    color: #555;\n    display: block;\n    font: normal 24px sans-serif;\n    outline: none;\n    background: transparent;\n    border: none;\n    cursor: pointer;\n    width: 15px;\n    text-align: center;\n}\n.datetime-picker > .month > .prev_next:hover {\n  background-color: #333;\n  color: #fff;\n}\n.datetime-picker > .month > .prev_next.prev {\n  float: left;\n}\n.datetime-picker > .month > .prev_next.next {\n  float: right;\n}\n.datetime-picker > .days {\n    width: 210px; /* 30 x 7 */\n    margin: 10px;\n    text-align: center;\n}\n.datetime-picker > .days .day-of-week,\n.datetime-picker > .days .day {\n    box-sizing: border-box;\n    -moz-box-sizing: border-box;\n    border: 1px solid transparent;\n    width: 30px;\n    line-height: 28px;\n    float: left;\n}\n.datetime-picker > .days .day-of-week {\n    font-weight: bold;\n}\n.datetime-picker > .days .day-of-week.weekend {\n    color: #ccc;\n    background-color: inherit;\n}\n.datetime-picker > .days .day:not(.selectable) {\n    color: #ccc;\n    cursor: default;\n}\n.datetime-picker > .days .weekend {\n    color: #ccc;\n    background-color: #eee;\n}\n.datetime-picker > .days .day.selectable  {\n    cursor: pointer;\n}\n.datetime-picker > .days .day.selected {\n    background: gray;\n    color: #fff;\n}\n.datetime-picker > .days .day:not(.selected).selectable:hover {\n    background: #eee;\n}\n.datetime-picker > .days:after {\n    content: '';\n    display: block;\n    clear: left;\n    height: 0;\n}\n.datetime-picker .hourLabel,\n.datetime-picker .minutesLabel {\n    display: inline-block;\n    width: 40px;\n    text-align: right;\n}\n.datetime-picker input[type=range] {\n    width: 200px;\n}\n  "
                     ],
@@ -130,6 +140,13 @@ var DateTimePickerComponent = (function () {
         'minuteStep': [{ type: core_1.Input, args: ['minuteStep',] },],
         'firstDayOfWeek': [{ type: core_1.Input, args: ['first-day-of-week',] },],
         'defaultValue': [{ type: core_1.Input, args: ['default-value',] },],
+        'minDate': [{ type: core_1.Input, args: ['min-date',] },],
+        'maxDate': [{ type: core_1.Input, args: ['max-date',] },],
+        'minHour': [{ type: core_1.Input, args: ['min-hour',] },],
+        'maxHour': [{ type: core_1.Input, args: ['max-hour',] },],
+        'disabledDates': [{ type: core_1.Input, args: ['disabled-dates',] },],
+        'changes': [{ type: core_1.Output, args: ['changes',] },],
+        'closing': [{ type: core_1.Output, args: ['closing',] },],
         '_hours': [{ type: core_1.ViewChild, args: ['hours',] },],
         '_minutes': [{ type: core_1.ViewChild, args: ['minutes',] },],
     };
