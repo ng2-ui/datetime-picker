1a62fdf7819ebe2fccaad408e98d3bc9bff59518
Allen Kim <allenhwkim@gmail.com>
2017-03-04 12:34:53 -0500

    0.14.4

--- dist/ng2-datetime.js
+++ dist/ng2-datetime.js
@@ -126,7 +126,8 @@ var Ng2Datetime = (function () {
         month: 'month',
         day: 'day',
         hour: 'hour',
-        minute: 'minute'
+        minute: 'minute',
+        currentTime: "current time"
     };
     Ng2Datetime.days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     Ng2Datetime.weekends = [0, 6];
